2daac79039e709223244678cbb546de4c378753b
LarisaYakovenko <133209659+LarisaYakovenko@users.noreply.github.com>
2024-01-01 21:10:46 +0200

    update tasck

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,48 @@
+class Key {
+    private signature: number = Math.random();
+    
+  getSignature(): number { return this.signature; }
+}
+
+class Person {
+  constructor(private key: Key) { }
+
+  getKey(): Key { return this.key }
+}
+
+abstract class House {
+  protected door: boolean = false 
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+ 
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Людина в будинку.');
+    } else {
+      console.log('Двері зачинені.');
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true; 
+      console.log('Двері відчинені.');
+    } else {
+      console.log('Двері зачинені.');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -8,4 +53,7 @@ house.openDoor(person.getKey());
 house.comeIn(person);
 
 
-export {};
+export { };
+
+
+
